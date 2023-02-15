454760ea54d965e777b3221b90cef2e8f07c78ef
Kate Bolshakova <katassr@mail.ru>
2023-02-15 12:08:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,45 +1,76 @@
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
+
 function decode(expr) {
-    let arr = expr.split("**********")
-let arr_Ten = []
-for (let j = 0; j < arr.length; j++) {
-    let arrSlice = []
-    for (let i = 0; i < arr[j].length; i +=10) {
-        arrSlice.push(arr[j].slice(i, i + 10));
-}
-arr_Ten.push(arrSlice)
-}
-let buc = []
-for (let x = 0; x < arr_Ten.length; x++) {//длина 3. | 0 
-    let slovo = []
-    for (i = 0; i < arr_Ten[x].length; i ++) { 
-        let sss = ''
-        for (j = 0; j < arr_Ten[x][i]; j +=2) {
-            if (arr_Ten[x][i][j] + arr_Ten[x][i][j +1] == 10) {
-                sss += "." 
-            } else if (arr_Ten[x][i][j] + arr_Ten[x][i][j +1] == 11) {
-                sss += "-" 
-            } 
+    let arr = []
+    let morse = []
+    let a = 0
+    let b = 10
+    while ( arr.length * 10 < expr.length) {
+        let arrSlice = expr.slice(a, b)
+        let str = ""
+        for (i = 0; i < 10;) {
+            if (arrSlice[i] + arrSlice[i+1] == "10") {
+                str += "." 
+             }
+             if (arrSlice[i] + arrSlice[i+1] == "11") {
+                str += "-" 
+            }
+            i += 2
         }
-        slovo.push(sss)
+        arr.push(arrSlice)
+        morse.push(str)
+        a += 10
+        b += 10
     }
-    buc.push(slovo)
-}
-console.log(buc)
-let test = []
-for (let i = 0; i < buc.length; i++) { //3
-    let bucArr = []
-    for (let j = 0; j < buc[i].length; j++) {//el
-    bucArr.push(MORSE_TABLE[buc[i][j]])
+    //console.log(morse)
+    let result = '';
+    for (x in morse) {
+        if (morse[x] in MORSE_TABLE) {
+            result += MORSE_TABLE[morse[x]];
+        } else {
+            result += " ";
+        }
     }
-test.push(bucArr)
+    return result
 }
-let res = []
-for (let i = 0; i < test.length; i++) {
-    res.push(test[i].join(''))
-}
-let rezult = res.join(" ")
-return rezult
-}
+
 
 module.exports = {
     decode
